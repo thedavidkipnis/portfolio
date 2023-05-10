c056373d7504889cc591cee52791551e94e217da
David Kipnis <thedavidkipnis@gmail.com>
2023-05-09 22:45:58 -0700

    Fixed a bunch of stuff

---
+++ displays.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = "Hi! I'm David";
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("intro_text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+  }
+
+  window.addEventListener('load', typeWriter)
