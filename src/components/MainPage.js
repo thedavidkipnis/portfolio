21146f37cc2e37e3cd094cabf34b3fc0884ac2ae
DK <thedavidkipnis@gmail.com>
2025-01-08 18:38:03 -0800

    All links in place

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,12 +2,14 @@ import InfoBlock from "./InfoBlock";
 
 const MainPage = () =>  {
 
+    const projectLinks = ['https://thedavidkipnis.github.io/Tondo/', 'https://thedavidkipnis.github.io/game-of-life/']
+    const researchLinks = ['https://ieeexplore.ieee.org/document/10216577']
     const contactLinks = ['https://www.linkedin.com/in/thedavidkipnis/', 'https://github.com/thedavidkipnis/']
 
     return (
       <div className="MainPage">
-        <InfoBlock text={'projects'} entries={[]}/>
-        <InfoBlock text={'research'} entries={[]}/>
+        <InfoBlock text={'projects'} entries={projectLinks}/>
+        <InfoBlock text={'research'} entries={researchLinks}/>
         <InfoBlock text={'contact'} entries={contactLinks}/>
       </div>
     );
