dd30f7afce1c2db84385ea36e319a40177fc3b4c
DK <thedavidkipnis@gmail.com>
2025-01-08 16:08:52 -0800

    Added basic expansion to InfoBlocks

--- src/components/InfoBlock.js
+++ src/components/InfoBlock.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 
 const InfoBlock = (props) => {
+
+    const [isExpanded, expandBlock] = useState(false);
+
+    const expandedStyle = {height: '8vh', width: '32vh'}
+    const nonExpanded = {height: '4vh'}
+
     return (
-      <div className="InfoBlock">
-        <a href={props.link}>{props.text}</a>
+      <div className="InfoBlock" 
+      style={isExpanded ? expandedStyle : nonExpanded}
+      onClick={() => expandBlock(!isExpanded)}>
+        {props.text}
       </div>
     );
   }
