2fee6b8b46bd1194f9c407973c4a567e202fc628
DK <thedavidkipnis@gmail.com>
2025-01-16 12:50:31 -0800

    Formatting changes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,13 +3,14 @@ import TextBlock from "./TextBlock";
 
 const MainPage = () =>  {
 
-    const introText = [['I\'m David, a fullstack engineer - I build web apps and database solutions!', '']]
+    const introText = [['I\'m a fullstack engineer', ''],['I build web apps and database solutions', '']] 
     const projectLinks = [['Tondo', 'https://thedavidkipnis.github.io/Tondo/'], ['Game of Life', 'https://thedavidkipnis.github.io/game-of-life/']]
     const researchLinks = [['Link to my paper!' ,'https://ieeexplore.ieee.org/document/10216577']]
     const contactLinks = [['LinkedIn' ,'https://www.linkedin.com/in/thedavidkipnis/'], ['GitHub', 'https://github.com/thedavidkipnis/']]
 
     return (
       <div className="MainPage">
+        <InfoBlock text={'about me'} entries={introText}/>
         <InfoBlock text={'projects'} entries={projectLinks}/>
         <InfoBlock text={'research'} entries={researchLinks}/>
         <InfoBlock text={'contact'} entries={contactLinks}/>
